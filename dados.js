36c51169f774b462704c2198684eda6fab965f03
Narydiniz <senainaryara@gmail.com>
2024-09-05 09:25:36 -0300

    Iniciando JavaScript

---
+++ dados.js
@@ -0,0 +1,18 @@
+let dados = [
+    {
+        titulo: "Rayssa Leal",
+        descrição: "Rayssa Leal, a Fadinha, é uma skatista brasileira que conquistou o mundo com seu talento e carisma. Aos poucos anos, ela já coleciona títulos e inspira milhões de jovens.",
+        link: "https://pt.wikipedia.org/wiki/Rayssa_Leal"
+    },
+    {
+        titulo: "Beatriz Souza",
+        descrição: "Beatriz Souza é uma judoca brasileira que se destaca pela sua técnica e determinação. Com diversas medalhas em competições nacionais e internacionais, ela é um dos grandes nomes do judô brasileiro.",
+        link: "https://[link para a página da Beatriz Souza]" // Substitua por um link válido
+      },
+      {
+        titulo: "Rebeca Andrade",
+        descrição: "Rebeca Andrade é uma ginasta brasileira que conquistou o mundo com suas acrobacias e força. Ela é considerada uma das maiores ginastas brasileiras de todos os tempos.",
+        link: "https://[link para a página da Rebeca Andrade]" // Substitua por um link válido
+      }
+];
+
