ce471fe03e38608d10bdb721b82afc779c34d258
NaryDiniz <senainaryara@gmail.com>
2024-09-06 12:26:44 -0300

    Atualizando o arquivo js

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
-console.log(dados);
+function pesquisar() {
+    // Seleciona a seção onde os resultados serão exibidos
+    let section = document.getElementById("resultados-pesquisa");
+   
+    // Inicializa uma string vazia para armazenar os resultados
+    let resultados = "";
+
+    // Itera sobre cada dado da pesquisa e constrói o HTML
+    for (let dado of dados) {
+        // Cria um novo elemento HTML para cada resultado
+        resultados += `
+            <div class="item-resultado">
+                <h2>
+                    <a href="#" target="_blank">${dado.titulo}</a>
+                </h2>
+                <p class="descricao-meta">${dado.descrição}</p>
+                <a href=${dado.link} target="_blank">Mais informações</a>
+            </div>
+        `;
+    }
+
+    // Atualiza o conteúdo da seção com os resultados construídos
+    section.innerHTML = resultados;
+}
+
+
+
+
--- dados.js
+++ dados.js
@@ -7,12 +7,12 @@ let dados = [
     {
         titulo: "Beatriz Souza",
         descrição: "Beatriz Souza é uma judoca brasileira que se destaca pela sua técnica e determinação. Com diversas medalhas em competições nacionais e internacionais, ela é um dos grandes nomes do judô brasileiro.",
-        link: "https://[link para a página da Beatriz Souza]" // Substitua por um link válido
+        link: "https://pt.wikipedia.org/wiki/Beatriz_Souza" 
       },
       {
         titulo: "Rebeca Andrade",
         descrição: "Rebeca Andrade é uma ginasta brasileira que conquistou o mundo com suas acrobacias e força. Ela é considerada uma das maiores ginastas brasileiras de todos os tempos.",
-        link: "https://[link para a página da Rebeca Andrade]" // Substitua por um link válido
+        link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade" 
       }
 ];
 
